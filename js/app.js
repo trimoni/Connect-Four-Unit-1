0b34f5964aa34e005781306cf559695444b98eda
Tal <trimoni8@gmail.com>
2022-09-08 09:01:25 -0400

    start of project

---
+++ js/app.js
@@ -0,0 +1,31 @@
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+// 1. Define the Variables in order to track the state of the game
+
+// 2. Make sure to add cached elements
+
+// 3. Initalize the game, function should call to render the game state
+
+// 4. Game needs to be rendered to user
+
+// 5. Name the Constants
+
+// 6. Need a HandleClick function so player can click on cells
+
+// 7. Need a function for a winner
+
+// 8. Reset function
+
+// 9. Computer Challenge
+
+// 10. Add Special designs to game
+
