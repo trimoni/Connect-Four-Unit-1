20e0927586c0e9884abc00ff5be92d5e5144974d
Tal <trimoni8@gmail.com>
2022-09-09 14:43:24 -0400

    grid connect 4

--- js/app.js
+++ js/app.js
@@ -1,31 +1,33 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*---------------------------- Variables (state) ----------------------------*/
+let board, player
 
 /*------------------------ Cached Element References ------------------------*/
+const circle = document.querySelectorAll('#board > div')
+const topMessage = document.querySelector('#message')
+console.log(circle)
+const resetButton = document.querySelector('#reset')
 
 /*----------------------------- Event Listeners -----------------------------*/
+
+
 
 /*-------------------------------- Functions --------------------------------*/
 
 
-// 1. Define the Variables in order to track the state of the game
 
-// 2. Make sure to add cached elements
+// Define the Variable in order to track the state of the game
+// Make sure to add cached elements for an ID that covers all the divs, a reset button function 
+// Need event listeners for click, reset, (computer-mode at the end)
+// Initialize an init function, put a const of board with 42 nulls
+// Game needs to be rendered to user
+// Name the Constant of winningcombos (find a way to avoid hard-coding, but if all else fails, hard-code) (look into window sliding method)
+// Need a Click function so player can click on divs
+// Need a function for a winner
+// Reset function
+// Add a Computer Challenge Mode
+// Add Special designs to game
 
-// 3. Initalize the game, function should call to render the game state
 
-// 4. Game needs to be rendered to user
-
-// 5. Name the Constants
-
-// 6. Need a HandleClick function so player can click on cells
-
-// 7. Need a function for a winner
-
-// 8. Reset function
-
-// 9. Computer Challenge
-
-// 10. Add Special designs to game
 
