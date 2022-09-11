26a09c107479505e4756d20a2f1ed8292a455755
Tal <trimoni8@gmail.com>
2022-09-11 16:05:06 -0400

    clean function order

--- js/app.js
+++ js/app.js
@@ -70,159 +70,103 @@ const winningArrays = [
   [11, 18, 25, 32],
   [12, 19, 26, 33],
   [13, 20, 27, 34],
-]
+];
 /*---------------------------- Variables (state) ----------------------------*/
-let board = Array(42).fill(null), winner, turn, tie
+let board = Array(42).fill(null),
+  winner,
+  turn,
+  tie;
 
 /*------------------------ Cached Element References ------------------------*/
-const circleSpace = document.querySelectorAll('section > div')
-console.log(circleSpace)
-const topMessage = document.querySelector('#message')
-const reset = document.getElementById("resetBtn")
+const circleSpace = document.querySelectorAll("section > div");
+const topMessage = document.querySelector("#message");
+const reset = document.getElementById("resetBtn");
 /*----------------------------- Event Listeners -----------------------------*/
-circleSpace.forEach((circle, index) => circle.addEventListener('click', () => handleClick(index)))
+circleSpace.forEach((circle, index) =>
+  circle.addEventListener("click", () => handleClick(index))
+  // we are clicking on a circle (div), the index is the value index of each circle (div).
+  // We are grabbing the index value from the circle (div) and we will move it around to other functions that will need to use it.
+);
 
-
-reset.addEventListener('click', init)
-// reset.addEventListener('click', () => {
-//   circleSpace.forEach(input => input.remove = '')
-// })
-
+reset.addEventListener("click", init);
 
 /*-------------------------------- Functions --------------------------------*/
 
-init()
+init();
 
 function init() {
-  board.fill(null)
-  winner = false
-  tie = false
-  turn = 1
+  board.fill(null);
+  winner = false;
+  tie = false;
+  turn = 1;
   for (let i = 0; i < circleSpace.length; i++) {
-    circleSpace[i].style.backgroundColor = 'white'
+    circleSpace[i].style.backgroundColor = "white";
   }
-  updateMessage()
+  // loop through the divs, and will give the index value of circleSpace a color of white for every new game/reset
+  updateMessage();
 }
-
 
 function handleClick(index) {
-  if ( winner) return
-  render(index)
+  if (winner) return;
+  // if winner is false, keep playing the game
+  render(index);
+  // as long as there isn't a winner, render function will activate
 }
 
-function checkIfWinner() {
-    winner = winningArrays.some(combo => Math.abs(board[combo[0]] + board[combo[1]] + board[combo[2]] + board[combo[3]]) === 4 )
-}
-
-function checkIfTie() {
-  if (!board.includes(null)) tie = true
-}
-
-function switchPlayerTurn() {
-  if (!winner) turn *= -1
+function getClassIndex(index) {
+  const bottomArray = [6, 5, 4, 3, 2, 1, 0];
+  // this targets the bottom row
+  let classIndex = bottomArray[index % 7];
+  let boot = document.querySelector(`#sq${classIndex}`);
+  // the bottom row, each value is getting a divisible of 7
+  let color = boot.style.backgroundColor;
+  while (color === "red" || color === "yellow") {
+    // grabbing the sq id's in the bottom row
+    classIndex += 7;
+    boot = document.querySelector(`#sq${classIndex}`);
+    color = boot.style.backgroundColor;
+  }
+  return classIndex;
 }
 
 function updateBoard(index) {
-  board[index] = turn
-
-  // for (let index in board) {
-    // console.log(index)
-      // if (board[index] === 1) circleSpace[index].style.backgroundColor = 'Red'
-      // else if (board[index] === -1) circleSpace[index].style.backgroundColor = 'Yellow'
-      // else if (board[index] === null)
-      // circleSpace[index].style.backgroundColor = 'White'
-      console.log(board)
-      // }
-      // circleSpace[index].style.backgroundColor = 'red'
- 
-      const backgroundColor = turn === 1 ? "red" : "yellow"
-      let circle = document.querySelector(`#sq${index}`)
-      circle.style.backgroundColor = backgroundColor
-      console.log(turn)
+  board[index] = turn;
+  const backgroundColor = turn === 1 ? "red" : "yellow";
+  let circle = document.querySelector(`#sq${index}`);
+  circle.style.backgroundColor = backgroundColor;
 }
+
+function checkIfWinner() {
+  winner = winningArrays.some(
+    (combo) =>
+      Math.abs(
+        board[combo[0]] + board[combo[1]] + board[combo[2]] + board[combo[3]]
+      ) === 4
+  );
+}
+
+function checkIfTie() {
+  if (!board.includes(null)) tie = true;
+}
+
+function switchPlayerTurn() {
+  if (winner === false) turn *= -1;
+}
+
 
 function updateMessage() {
-  topMessage.innerText = `It's ${turn > 0 ? 'Red' : 'Yellow'}s turn`
-  if (tie) topMessage.innerText = `Tie game`
-  if (winner) topMessage.innerText = `${turn > 0 ? 'Red' : 'Yellow'} wins`
+  topMessage.innerText = `It's ${turn > 0 ? "Red" : "Yellow"}s turn`;
+  if (tie) topMessage.innerText = `Tie game`;
+  if (winner) topMessage.innerText = `${turn > 0 ? "Red" : "Yellow"} wins`;
 }
 
 
-function render(par) {
-//  console.log(par % 7)
-
-  const magicArray  = [6,5,4,3,2,1,0]
-// this targets the bottom rom
-  let classIndex = magicArray[par%7]
-  let boot = document.querySelector(`#sq${classIndex}`)
-  // the bottom row, each value is getting a divisible of 7
-  let color = boot.style.backgroundColor
-  while (color === "red" ||  color === "yellow"){
-    // grabbing the sq id's in the bottom row
-    // console.log(boot.style.backgroundColor)
-    
-    classIndex += 7
-    boot = document.querySelector(`#sq${classIndex}`)
-    color = boot.style.backgroundColor
-
-    console.log(document.querySelector(`#sq${classIndex}`))
-  
-}
-  console.log(color)
-  
-  
-
-  updateBoard(classIndex)
-  checkIfTie()
-  checkIfWinner()
-  switchPlayerTurn()
-  updateMessage()
+function render(index) {
+  const classIndex = getClassIndex(index);
+  updateBoard(classIndex);
+  checkIfTie();
+  checkIfWinner();
+  switchPlayerTurn();
+  updateMessage();
 }
 
-  // board.forEach((circle, sqIdx) => {
-  //   if (circle === 1) {
-  //     circleSpace[sqIdx].textContent = circleSpace[sqIdx].classList.add('red-piece')
-  //   } if (circle === -1) {
-  //     circleSpace[sqIdx].classList.add('yellow-piece')
-  //   } else if (circle === null) {
-  //     circleSpace[sqIdx].textContent = ''
-  //   }
-  // })
-  //   if (winner === null){
-  //     topMessage.textContent = `Player ${player === 1 ? "red's" : "yellow's"} turn`
-  //     //make tie function later
-  //   } else {
-  //     topMessage.textContent = `Player ${player === 1 ? "red" : "yellow"} wins`
-  //   }
-
-
-// function handleClick(evt) {
-//   let index = parseInt(evt.target.id.replace('sq', ''))
-//   console.log(index)
-  // if(board[index] || winner) {
-  //   return
-  // }
-  // board[index] = player
-  // player *= -1
-  // winner = gotWinner()
-  // render()
-
-// Need event listeners for click, reset, (computer-mode at the end)
-// Game needs to be rendered to user
-// Name the Constant of winningcombos (find a way to avoid hard-coding, but if all else fails, hard-code) (look into window sliding method)
-// Need a Click function so player can click on divs
-// Need a function for a winner
-// Reset function
-// Add a Computer Challenge Mode
-// Add Special designs to game
-
-
-
-
-
-// [1, 2, 3, 4, 5, 6, 7],
-//           [8, 9, 10, 11, 12, 13, 14],
-//           [15, 16, 17, 18, 19, 20, 21],
-//           [22, 23, 24, 25, 26, 27, 28],
-//           [29, 30, 31, 32, 33, 34, 35],
-//           [36, 37, 38, 39, 40, 41, 42]
